5b5a1f22a268149c4f1f83305479d42b762af3f2
Christian Catrofe <christianlopes100@outlook.com>
2024-04-27 17:51:00 -0300

    FEAT: Implementando logs utilizando o Slf4j with lombok

--- src/main/java/com/br/avaliationsystemecommerce/adpter/AvaliationControllerAdapter.java
+++ src/main/java/com/br/avaliationsystemecommerce/adpter/AvaliationControllerAdapter.java
@@ -14,6 +14,7 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,6 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/avaliations")
 @AllArgsConstructor
@@ -34,6 +36,7 @@ public class AvaliationControllerAdapter implements AvaliationControllerPort {
 
     @Override
     public ResponseEntity<?> createAvaliation(AvaliationRequest avaliationRequest) {
+        log.info("Creating a new avaliation for product {}", avaliationRequest.productId());
         avaliationServicePersistence.createAvaliation(avaliationRequest);
         return ResponseEntity.status(HttpStatus.ACCEPTED).build();
     }
@@ -43,12 +46,14 @@ public class AvaliationControllerAdapter implements AvaliationControllerPort {
             @PathVariable Long productId,
             @RequestParam(name = "page", defaultValue = "0", required = false) @Min(0) Integer page
     ) {
+        log.info("Getting comments for product {}", productId);
         AvaliationCommentsOutput comments = avaliationServiceReading.getCommentsProduct(productId, page);
         return ResponseEntity.ok(comments);
     }
 
     @Override
     public ResponseEntity<AvaliationAverageOutput> getAverageProduct(Long productId) {
+        log.info("Getting average for product {}", productId);
         AvaliationAverageOutput response = avaliationServiceReading.getAverageProduct(productId);
         return ResponseEntity.ok(response);
     }
--- src/main/java/com/br/avaliationsystemecommerce/service/AvaliationServiceReading.java
+++ src/main/java/com/br/avaliationsystemecommerce/service/AvaliationServiceReading.java
@@ -6,6 +6,7 @@ import com.br.avaliationsystemecommerce.dto.AvaliationCommentsOutput;
 import com.br.avaliationsystemecommerce.dto.MetaData;
 import com.br.avaliationsystemecommerce.port.AvaliationRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -14,6 +15,7 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.List;
 
+@Slf4j
 @Service
 @AllArgsConstructor
 public class AvaliationServiceReading {
@@ -22,21 +24,32 @@ public class AvaliationServiceReading {
 
     @Cacheable(value = "comments_product", key = "#productId + 0")
     public AvaliationCommentsOutput getCommentsProduct(Long productId, Integer page) {
+        log.info("Getting comments for product {}, Page {}", productId, page);
         Pageable pageable = Pageable.ofSize(10).withPage(page);
-        List<Avaliation> comments = avaliationRepository.findByProductId(productId, pageable);
-        return new AvaliationCommentsOutput(
-                new MetaData(page, 10),
-                comments
-        );
+        try{
+            List<Avaliation> comments = avaliationRepository.findByProductId(productId, pageable);
+            return new AvaliationCommentsOutput(
+                    new MetaData(page, 10),
+                    comments
+            );
+        } catch (Exception e) {
+            log.error("Error getting comments for product {}", productId);
+            throw  new RuntimeException("Error getting comments for product " + productId, e);
+        }
     }
 
     @Cacheable(value = "average_product", key = "#productId")
     public AvaliationAverageOutput getAverageProduct(Long productId) {
-        BigDecimal average = avaliationRepository.getAverageProduct(productId);
-        if (average == null) {
-            return new AvaliationAverageOutput(BigDecimal.ZERO, productId);
+        try{
+            BigDecimal average = avaliationRepository.getAverageProduct(productId);
+            if (average == null) {
+                log.info("No avaliations for product {}", productId);
+                return new AvaliationAverageOutput(BigDecimal.ZERO, productId);
+            }
+            return new AvaliationAverageOutput(average.setScale(2, RoundingMode.HALF_UP), productId);
+        } catch (Exception e) {
+            log.error("Error getting average for product {}", productId);
+            throw  new RuntimeException("Error getting average for product " + productId, e);
         }
-
-        return new AvaliationAverageOutput(average.setScale(2, RoundingMode.HALF_UP), productId);
     }
 }
